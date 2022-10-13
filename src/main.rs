7f367c7b518bfc4e7b00bb8c21a0f6bf1f539d1a
7ap <109433659+7ap@users.noreply.github.com>
2022-10-13 17:18:25 -0500

    feat: stub out the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Path to target image
+    file: PathBuf,
+
+    /// Vanity hash (in the URL)
+    hash: Option<String>,
+}
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
