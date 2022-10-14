56f86369db4e67e084b3dae4b15c3207f8c6a26d
7ap <109433659+7ap@users.noreply.github.com>
2022-10-13 22:31:45 -0500

    feat: add expression encoder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use image::GenericImageView;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Expression {
+    r#type: String,
+    id: usize,
+    color: String,
+    latex: String,
+    #[serde(rename = "fillOpacity")]
+    fill_opacity: String,
+    #[serde(rename = "lineOpacity")]
+    line_opacity: String,
+    #[serde(rename = "lineWidth")]
+    line_width: String,
+}
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -22,6 +38,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     if cli.file.metadata().unwrap().len() > 5000000 {
         panic!("File must be less than 5 megabytes.")
+    }
+
+    let image = image::open(cli.file).unwrap();
+
+    let mut expressions: Vec<Expression> = Vec::new();
+
+    for (x, y, pixel) in image.pixels() {
+        let expression = Expression {
+            r#type: String::from("expression"),
+            id: expressions.len(),
+            color: format!("rgb({}, {}, {})", pixel.0[0], pixel.0[1], pixel.0[2]),
+            #[rustfmt::skip]
+            latex: format!("{}\\le x\\le{}\\left\\{{{}\\le y\\le{}\\right\\}}", y, y + 1, x, x + 1),
+            fill_opacity: String::from(""),
+            line_opacity: String::from(""),
+            line_width: String::from(""),
+        };
+
+        expressions.push(expression);
     }
 
     Ok(())
