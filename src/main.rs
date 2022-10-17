fce752233eaeab4f31bbc59ecc203949483e4f32
7ap <109433659+7ap@users.noreply.github.com>
2022-10-17 12:51:57 -0500

    feat: add some error checking

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Path to target image
-    image: PathBuf,
+    /// Path to target file
+    file: PathBuf,
 
     /// Optional vanity hash
     hash: Option<String>,
@@ -15,6 +15,28 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+    let file = args.file;
+    let hash = args.hash;
+
+    if !file.exists() {
+        panic!("File does not exist.")
+    }
+
+    if file.extension().unwrap() != "png" {
+        panic!("File must be a PNG.")
+    }
+
+    if hash.is_some() {
+        let hash = hash.as_ref().unwrap();
+
+        if hash.len() != 10 {
+            panic!("Hash must be 10 characters long.")
+        }
+
+        if hash.chars().any(|char| !char.is_ascii_alphanumeric()) {
+            panic!("Hash must be an ASCII alphanumeric string.")
+        }
+    }
 
     Ok(())
 }
