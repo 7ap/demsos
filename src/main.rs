feacd406dbef85826491393c159b8ab99aa0e6bc
7ap <109433659+7ap@users.noreply.github.com>
2022-10-17 12:15:34 -0500

    feat: add clap arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to target image
+    image: PathBuf,
+
+    /// Optional vanity hash
+    hash: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
